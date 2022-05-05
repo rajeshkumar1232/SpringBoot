7d97665a0cf294f03c77cbf4cf2fe54f9220f29b
Rajesh kumar <47448471+rajeshkumar1232@users.noreply.github.com>
2022-05-05 16:24:01 +0530

    Simple springboot mvc app

---
+++ src/main/java/com/v2/practice/V4Application.java
@@ -0,0 +1,13 @@
+package com.v2.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(V4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/v2/practice/V4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.v2.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class V4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
